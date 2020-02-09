4fc6b8af2634c772f95260c808748db4851a20e0
SpoonJoon <joonhwan.yoo@gmail.com>
2020-02-09 11:31:04 -0500

    hackathon?

---
+++ SAXY.js
@@ -0,0 +1,7 @@
+
+// function(){
+//     var myAudio = new Audio();
+//     myAudio.src = chrome.extension.getURL("/Users/bigmac/Desktop/sTenGER/SAXY1.mp3");
+//     myAudio.play();
+    
+// }
---
+++ content.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function (request) {
+        var images = document.getElementsByTagName("img");
+
+       
+        for (var i = 0; i < images.length; i++) {
+            if( images[ i ].tagName == "IMG" )
+            {
+                images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/p960x960/84650333_2548238895423666_1570520908508954624_o.jpg?_nc_cat=101&_nc_oc=AQlFo-BLW6BrgjOY7S8TV2Zk-2cmhEv4gXEFrJM9bCtpyGXCDKHM1AR3phpAFFLbGpg&_nc_ht=scontent-lga3-1.xx&_nc_tp=6&oh=30765be0c26830c16eae7f03ed23efa2&oe=5ED4B99F";
+            }
+            document.body.style.background = "pink";
+}})
