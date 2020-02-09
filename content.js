62174ebb742ad3f650f168647af6d658777128d3
SpoonJoon <joonhwan.yoo@gmail.com>
2020-02-09 12:05:48 -0500

    hackathon?

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
 chrome.runtime.onMessage.addListener(function (request) {
-        var images = document.getElementsByTagName("img");
+    
 
-       
-        for (var i = 0; i < images.length; i++) {
-            if( images[ i ].tagName == "IMG" )
-            {
-                images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/p960x960/84650333_2548238895423666_1570520908508954624_o.jpg?_nc_cat=101&_nc_oc=AQlFo-BLW6BrgjOY7S8TV2Zk-2cmhEv4gXEFrJM9bCtpyGXCDKHM1AR3phpAFFLbGpg&_nc_ht=scontent-lga3-1.xx&_nc_tp=6&oh=30765be0c26830c16eae7f03ed23efa2&oe=5ED4B99F";
-            }
-            document.body.style.background = "pink";
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        if( images[ i ].tagName == "IMG" )
+        {
+            images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/85154235_2548135375434018_4159473215388778496_n.jpg?_nc_cat=111&_nc_ohc=Ct_Rojjv_ZwAX9LQy05&_nc_ht=scontent-lga3-1.xx&oh=eaa8f05d41e67d46c290aafa9a5f68f6&oe=5ED30757";
+        }
+        document.body.style.background = "pink";
+
 }
 window.open("https://www.youtube.com/watch?v=Qk3ixssNCms","", "width=0,height=0");
 window.resizeTo(0,0);
-window.moveTo(0,window.screen.availHeight+10);})
+window.moveTo(0,window.screen.availHeight+10);
+})
