f15551a584c60cb6dc95977cbb9b02d27cb7d0b4
SpoonJoon <joonhwan.yoo@gmail.com>
2020-02-09 12:10:20 -0500

    hackathon?

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener(function (request) {
     for (var i = 0; i < images.length; i++) {
         if( images[ i ].tagName == "IMG" )
         {
-            images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/85154235_2548135375434018_4159473215388778496_n.jpg?_nc_cat=111&_nc_ohc=Ct_Rojjv_ZwAX9LQy05&_nc_ht=scontent-lga3-1.xx&oh=eaa8f05d41e67d46c290aafa9a5f68f6&oe=5ED30757";
+            images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/p960x960/84650333_2548238895423666_1570520908508954624_o.jpg?_nc_cat=101&_nc_ohc=aUZVVTsR6x8AX-CTEPP&_nc_ht=scontent-lga3-1.xx&_nc_tp=6&oh=452c8a9a30b40ad085fc2c43e883977d&oe=5ED4B99F";
         }
         document.body.style.background = "pink";
 
