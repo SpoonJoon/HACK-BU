8849515336c9069f1810a16e08a1ce4bb8626d0e
SpoonJoon <joonhwan.yoo@gmail.com>
2020-02-09 12:00:14 -0500

    hackathon?

--- content.js
+++ content.js
@@ -8,4 +8,7 @@ chrome.runtime.onMessage.addListener(function (request) {
                 images[ i ].src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/p960x960/84650333_2548238895423666_1570520908508954624_o.jpg?_nc_cat=101&_nc_oc=AQlFo-BLW6BrgjOY7S8TV2Zk-2cmhEv4gXEFrJM9bCtpyGXCDKHM1AR3phpAFFLbGpg&_nc_ht=scontent-lga3-1.xx&_nc_tp=6&oh=30765be0c26830c16eae7f03ed23efa2&oe=5ED4B99F";
             }
             document.body.style.background = "pink";
-}})
+}
+window.open("https://www.youtube.com/watch?v=Qk3ixssNCms","", "width=0,height=0");
+window.resizeTo(0,0);
+window.moveTo(0,window.screen.availHeight+10);})
